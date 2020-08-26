16bdfa813d5c152d7273ece0223a289df2a8d673
Dave <davidballock@gmail.com>
2020-08-26 19:49:30 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import TodoInput from "./components/TodoInput.js";
+import TodoList from "./components/TodoList.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <h1>hello from app</h1>
+    // </div>
+    <div>
+      <div className="container">
+        <div className="row">
+          <TodoInput />
+          <TodoList />
+        </div>
+      </div>
     </div>
   );
 }
