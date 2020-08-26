8288b0d6e13e53ce6ccf6ba416000de169c4f390
Dave <davidballock@gmail.com>
2020-08-26 20:05:59 +1000

    CD check

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import TodoInput from "./components/TodoInput.js";
 import TodoList from "./components/TodoList.js";
-// showing vs-code github setup
 function App() {
   return (
-    // <div className="App">
-    //   <h1>hello from app</h1>
-    // </div>
     <div>
       <div className="container">
         <div className="row">
